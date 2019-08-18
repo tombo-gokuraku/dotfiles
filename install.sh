ad145ce6dc43c4875ec541926bcaac110f49fb56
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-08-18 19:57:09 +0900

    add install list

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 cat ./apt_install_list.txt  | xargs sudo apt install -y
-cat ./pip_install_list.txt  | xargs pip install --user
+cat ./pip_install_list.txt  | xargs sudo pip3 install
+cat ./npm_install_list.txt  | xargs sudo npm -g install
+
