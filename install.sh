b18a782edd26fb54e9a38cf5e8355269d196e75b
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-08-25 23:20:28 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo apt update && sudo apt upgrade -y
 cat ./apt_install_list.txt  | xargs sudo apt install -y
 cat ./pip_install_list.txt  | xargs pip3 install --user
-cat ./npm_install_list.txt  | xargs npm -g install
+cat ./npm_install_list.txt  | xargs sudo npm -g install
 
