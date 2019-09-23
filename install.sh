f6b6441fe839ccabddeab6f263a6847589e02e4c
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-09-23 12:16:15 +0900

    create nvim python environmet shell script

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@ sudo apt update && sudo apt upgrade -y
 cat ./apt_install_list.txt  | xargs sudo apt install -y
 pip3 install --user -r pip_install_list.txt
 cat ./npm_install_list.txt  | xargs sudo npm -g install
+
+# create nvim_env
+python3 -m venv ~/Env/python_env/nvim_env
+source ~/Env/python_env/nvim_env/bin/activate
+pip3 install -r nvim_env_requirements.txt
+deactivate
