61995f6262ad4df0bbc8c555dfd0b9da0145d006
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2019-10-26 16:50:21 +0900

    add gpakosz .tmux.conf

--- link.sh
+++ link.sh
@@ -2,3 +2,5 @@
 ln -snfv ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -snfv ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -snfv ~/dotfiles/.config/nvim/ftplugin/javascript.vim ~/.config/nvim/ftplugin/javascript.vim
+ln -snfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snfv ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
