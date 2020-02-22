d665bb79326150e6d03e470702273a45cbb5ad1d
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-02-22 15:51:23 +0900

    try hyper-one-dark

--- .hyper.js
+++ .hyper.js
@@ -147,7 +147,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    // hyperをone-darkで表示してくれるプラグイン フォルダとリンクが同じ色で表示されるのが気に食わない
+    // "hyper-one-dark"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
