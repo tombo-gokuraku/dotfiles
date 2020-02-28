e98ce73dd382c8e978253d1e53e4cfbc5daf621d
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-02-28 17:19:20 +0900

    add cargo install scripts

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 sudo apt update && sudo apt upgrade -y
 cat ./apt_install_list.txt  | xargs sudo apt install -y
 pip3 install --user -r pip_install_list.txt
-cat ./npm_install_list.txt  | xargs sudo npm -g install
 cat ./snap_install_list.txt  | xargs sudo snap install
+cat ./cargo_install_list.txt  | xargs sudo cargo install -j4
+
+# install nodejs
+cat ./npm_install_list.txt  | xargs sudo npm -g install
 
 # create nvim_env
 python3 -m venv ~/Env/python_env/nvim_env
