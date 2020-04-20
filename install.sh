4b26b468c8586c1a4242e9c68667fe05ab28a0cc
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-04-20 18:26:57 +0900

    add rustup & cargo install commands

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@ sudo apt update && sudo apt upgrade -y
 cat ./apt_install_list.txt  | xargs sudo apt install -y
 pip3 install --user -r pip_install_list.txt
 cat ./snap_install_list.txt  | xargs sudo snap install
-cat ./cargo_install_list.txt  | xargs sudo cargo install -j4
+
+# install rustup & cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+cat ./cargo_install_list.txt  | xargs cargo install -j4
 
 # install nodejs
 cat ./npm_install_list.txt  | xargs sudo npm -g install
