aba713a97224c19df8d58133cf7a18ff8b9234d6
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-04 22:59:54 +0900

    add comment

--- install_font.sh
+++ install_font.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# install nerd font for vim-devicons
 FONT_DIRECTORY="$HOME/.local/share/fonts"
-
 FONT_NAME="Droid Sans Mono for Powerline Nerd Font Complete.otf"
 FONT_URL="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
 
+# create fonts directory
 if [ ! -d $FONT_DIRECTORY ] ; then
   echo "Make fonts directory at $FONT_DIRECTORY"
   mkdir -p $FONT_DIRECTORY
 fi
 
+# download fonts and update cache
 if [ ! -f "$FONT_DIRECTORY/$FONT_NAME" ] ; then
   cd $FONT_DIRECTORY && echo $(pwd) && curl -fLo "$FONT_NAME" $FONT_URL && cd - && fc-cache
 fi
