f0a8cf18d5d09caa368c54c11f22f46d9d03a8f8
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-09-02 21:22:18 +0900

    refactor: Split npm install shell from ./install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,7 @@ bash ./install_snap.sh
 bash ./install_cargo.sh
 
 # install nvm & Node.js & npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts #install LTS version Node.js
-# install npm applications
-nvm use --lts
-cat ./npm_install_list.txt  | xargs npm -g install
+bash ./install_npm.sh
 
 # create nvim_env
 python3 -m venv ~/Env/python_env/nvim_env
