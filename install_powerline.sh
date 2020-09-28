85c1ca511d2bf201db08eddafacb8c63301db355
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-09-28 13:23:12 +0900

    remove: Remove powerline shell prompt

--- install_powerline.sh
+++ install_powerline.sh
@@ -2,14 +2,19 @@
 # Run install_apt.sh and install_pip.sh before run this script.
 
 # get powerline install path
-PYTHON_SITE_PACKAGES_PATH=$(python3 -m site --user-site | sed "s/\/home\/$(whoami)/\~/")
-POWERLINE_BASH_PATH="$PYTHON_SITE_PACKAGES_PATH/powerline/bindings/bash/powerline.sh"
-
-grep $POWERLINE_BASH_PATH ~/.bashrc
-
-if [ $? == 0 ]; then
-  echo "powerline bash script sourced successfully."
-else
-  echo "source $POWERLINE_BASH_PATH"
-  echo "[ -f $POWERLINE_BASH_PATH ] && source $POWERLINE_BASH_PATH" >> ~/.bashrc
-fi
+########################################################
+# Disable powerline bash prompt.
+# Because it is too heavy to use.
+# Use starship instead.
+########################################################
+# PYTHON_SITE_PACKAGES_PATH=$(python3 -m site --user-site | sed "s/\/home\/$(whoami)/\~/")
+# POWERLINE_BASH_PATH="$PYTHON_SITE_PACKAGES_PATH/powerline/bindings/bash/powerline.sh"
+#
+# grep $POWERLINE_BASH_PATH ~/.bashrc
+#
+# if [ $? == 0 ]; then
+#   echo "powerline bash script sourced successfully."
+# else
+#   echo "source $POWERLINE_BASH_PATH"
+#   echo "[ -f $POWERLINE_BASH_PATH ] && source $POWERLINE_BASH_PATH" >> ~/.bashrc
+# fi
